9729c2aa3440e128613af547538ed8845d87805d
Guru Prasad <gurupras@buffalo.edu>
2013-06-05 15:29:54 -0400

    Fully implemented Constraint class

--- src/internal/containers/Constraint.java
+++ src/internal/containers/Constraint.java
@@ -1,5 +1,31 @@
 package internal.containers;
 
-public class Constraint {
+import internal.containers.condition.ICondition;
 
+public class Constraint implements IContainer {
+	private Query query;
+	private ICondition condition;
+	
+	public Constraint(Query query, ICondition condition) {
+		this.query		= query;
+		this.condition	= condition;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "FOR ALL " + query.toString() + " ASSERT " + condition.toString();
+	}
+	
+	@Override
+	public String debugString() {
+		StringBuffer result = new StringBuffer("CONSTRAINT(FOR ALL ");
+		
+		result.append(query.debugString());
+		result.append(" ASSERT ");
+		result.append(condition.debugString());
+		result.append(")");
+		
+		return result.toString();
+	}
 }
