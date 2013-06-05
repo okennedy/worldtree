6825cc8e9971e2a7614d73fb1a4b8fa594a6ea06
Guru Prasad <gurupras@buffalo.edu>
2013-06-05 01:48:28 -0400

    Added TestParser JavaCC file for carrying out debugging

---
+++ src/test/parser/Direction.java
@@ -0,0 +1,33 @@
+package test.parser;
+
+public enum Direction {
+	UP("UP"),
+	DOWN("DOWN"),
+	LEFT("LEFT"),
+	RIGHT("RIGHT"),
+	;
+	
+	private final String direction;
+	
+	private Direction(String direction) {
+		this.direction = direction;
+	}
+	
+	protected static Direction getDirection(String string) {
+		for(Direction d : values()) {
+			if(d.toString().equalsIgnoreCase(string) || d.toString().substring(0, 0).equalsIgnoreCase(string))
+				return d;
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return direction;
+	}
+	
+	public String text() {
+		return direction;
+	}
+}
