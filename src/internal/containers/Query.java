a2b3c4558287e6334e248417db49b2cd9e0c7aa5
Guru Prasad <gurupras@buffalo.edu>
2013-06-05 00:31:30 -0400

    Query class reflects changes made to condition package

--- src/internal/containers/Query.java
+++ src/internal/containers/Query.java
@@ -1,12 +1,14 @@
 package internal.containers;
 
+import internal.containers.Pattern.Pattern;
 import internal.containers.condition.Condition;
+import internal.containers.condition.ICondition;
 
 public class Query {
 	private Pattern pattern;
-	private Condition condition;
+	private ICondition condition;
 	
-	public Query(Pattern pattern, Condition condition) {
+	public Query(Pattern pattern, ICondition condition) {
 		this.pattern	= pattern;
 		this.condition	= condition;
 	}
