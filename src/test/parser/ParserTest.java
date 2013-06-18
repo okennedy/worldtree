a46a18d46c5453867f971babf9faf56aef8e1d57
Guru Prasad <gurupras@buffalo.edu>
2013-06-17 23:26:12 -0400

    Added queryResolutionTest to ParserTest

--- src/test/parser/ParserTest.java
+++ src/test/parser/ParserTest.java
@@ -4,18 +4,61 @@ import static org.junit.Assert.*;
 
 import internal.containers.IContainer;
 import internal.containers.IStatement;
+import internal.containers.query.IQuery;
 import internal.parser.ParseException;
 import internal.parser.Parser;
+import internal.parser.resolve.ResolutionEngine;
+import internal.piece.PieceFactory;
+import internal.tree.IWorldTree.IMap;
+import internal.tree.WorldTreeFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class ParserTest {
+import static test.ui.UIDebugEngine.write;
 
+public class ParserTest {
+	private static IMap map;
+	private static WorldTreeFactory factory;
+	private static String[] pieceStrings = {
+		"LR",
+		"UD",
+		"UL",
+		"L",
+		"U",
+		"D",
+		"R",
+		"UDLR",
+		"UDL",
+		"UDR",
+		"ULR",
+		"DLR",
+		"UR",
+		"DR",
+		"DL",
+		""
+	};
+	
+	@BeforeClass
+	public static void setUp() {
+		try {
+			new PieceFactory(pieceStrings);
+			WorldTreeFactory factory = new WorldTreeFactory();
+			map = factory.newMap("TestMap", null, null);
+			map.fullInit();
+			write(map);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 	@Test
 	public void basicIOTest() {
 		try {
@@ -45,4 +88,33 @@ public class ParserTest {
 		}
 	}
 
+	@Test
+	public void queryResolutionTest() {
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			
+			StringBuffer command = new StringBuffer();
+			while(true) {
+				String cmd = in.readLine();
+				
+				if(cmd.equalsIgnoreCase("quit") || cmd.equalsIgnoreCase("exit"))
+					break;
+				
+				command.append(cmd);
+				if(command.toString().contains(";")) {
+					Parser parser = new Parser(new StringReader(command.toString()));
+					IQuery query = (IQuery) parser.parse();
+					System.out.println(query.debugString());
+					ResolutionEngine.resolve(map, query);
+					command.delete(0, command.length());
+				}
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+			fail();
+		} catch (ParseException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
 }
