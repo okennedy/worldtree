9f9ddc9dd5bc3c11e323ce730018d8dad7400cca
Guru Prasad <gurupras@buffalo.edu>
2013-07-21 11:34:45 -0400

    Rewrote QueryTest class to be more flexible

--- src/test/engine/QueryTest.java
+++ src/test/engine/QueryTest.java
@@ -3,10 +3,15 @@ package test.engine;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.StringReader;
 import java.util.Arrays;
 import java.util.List;
@@ -25,12 +30,13 @@ import internal.tree.WorldTreeFactory;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static internal.Helper.multiLine;
 
 public class QueryTest {
 	private IMap map;
 	private WorldTreeFactory factory = null;
 	private Parser parser			 = new Parser(new StringReader(""));
-	private String queryDirPath		 = "src/test/engine/query tests/";
+	private String queryTestsDir	 = "tests/query tests/";
 	private static String[] pieceStrings = {
 		"LR",
 		"UD",
@@ -61,26 +67,111 @@ public class QueryTest {
 	
 
 	@Test
-	public void inbuiltPropertiesTest() {
+	public void test() {
 //		Direction tests
-		factory = new WorldTreeFactory("src/test/engine/query tests/inbuilt properties/init.properties", "config");
-		map = factory.newMap("InbuiltPropertiesTestMap", null, null);
-		map.fullInit();
-		write(map);
-		String string = "A toeast B";
-		parser.ReInit(new StringReader(string));
-		try {
-			IQuery query = parser.query();
-			System.out.println(ResolutionEngine.evaluate(map, query));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		File dir = new File(queryTestsDir);
+		
+		StringBuffer errorMessage = new StringBuffer();
+		for(File testDir : dir.listFiles()) {
+			boolean failed = false;
+			Result result = null, storedResult = null;
+			
+			errorMessage.delete(0, errorMessage.length());
+			
+			File testFile = new File(testDir + "/init.properties");
+			if(!testFile.exists()) {
+				errorMessage.append("\t" + testDir.getName() + " :init.properties file does not exist!\n");
+				failed = true;
+			}
+			
+			if(!failed) {
+				factory = new WorldTreeFactory(testDir.getAbsolutePath() + "/init.properties");
+				map = factory.newMap("InbuiltPropertiesTestMap", null, null);
+				map.fullInit();
+				write(map);
+				try {
+					testFile = new File(testDir + "/test");
+					if(!testFile.exists()) {
+						errorMessage.append("\t" + testDir.getName() + " :test file does not exist!\n");
+						failed = true;
+					}
+					
+					if(!failed) {
+						BufferedReader in = new BufferedReader(new FileReader(testFile));
+						String line = null;
+						
+						while( (line = in.readLine()) != null) {
+							if(line.trim().startsWith("#") || line.length() < 1)
+								continue;
+							parser.ReInit(new StringReader(line));
+							IQuery query = parser.query();
+							result = ResolutionEngine.evaluate(map, query);
+						}
+					}
+					
+					testFile = new File(testDir + "/result.obj");
+					if(!testFile.exists()) {
+						errorMessage.append("\t" + testDir.getName() + " :result.obj file does not exist!\n");
+						failed = true;
+					}
+					if(!failed) {
+						ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(testDir + "/result.obj")));
+						storedResult = (Result) ois.readObject();
+						ois.close();
+					}
+					
+					failed = !result.toString().equals(storedResult.toString());
+					
+					if(failed) {
+						errorMessage.append("\t" + testDir.getName() + " :Results do not match!\n");
+						
+						testFile = new File(testDir + "/result");
+						if(!testFile.exists())
+							errorMessage.append("\t" + testDir.getName() + " :result file does not exist!\n");
+						else {
+							StringBuffer storedResultString = new StringBuffer();
+							BufferedReader in = new BufferedReader(new FileReader(testFile));
+							
+							String line = null;
+							while( (line = in.readLine()) != null)
+								storedResultString.append(line + "\n");
+							in.close();
+							
+							errorMessage.append(multiLine(Arrays.asList(new String[] {
+							"Current Result\n" + result.toString(), "     ", "Stored Result\n" + storedResult.toString()		
+							})));
+							errorMessage.append("\n");
+						}
+					}
+				} catch (ParseException e) {
+					errorMessage.append("\t" + testDir.getName() + " :" + e.getMessage() + "\n");
+					failed = true;
+					e.printStackTrace();
+				} catch (FileNotFoundException e) {
+					errorMessage.append("\t" + testDir.getName() + " :" + e.getMessage() + "\n");
+					failed = true;
+				} catch (IOException e) {
+					errorMessage.append("\t" + testDir.getName() + " :" + e.getMessage() + "\n");
+					failed = true;
+				} catch (ClassNotFoundException e) {
+					errorMessage.append("\t" + testDir.getName() + " :" + e.getMessage() + "\n");
+					failed = true;
+					e.printStackTrace();
+				}
+			}
+			if(failed) {
+				System.err.println(testDir.getName() + " :Failed!");
+				System.err.println(errorMessage.toString());
+			}
+			else
+				System.out.println(testDir.getName() + " :Passed!");
 		}
 	}
 	
 	@Test
 	public void createNewQueryTest() {
-		factory = new WorldTreeFactory();
+		String testDir = null;
+		factory = new WorldTreeFactory("init.properties");
 		Properties properties = factory.properties();
 		System.out.println("Query :");
 		try {
@@ -93,14 +184,15 @@ public class QueryTest {
 			System.out.println("\nComments (End with ';') :");
 			
 //			Comments for this test
-			StringBuffer comments = new StringBuffer("#");
+			StringBuffer comments = new StringBuffer();
 			while(!comments.toString().contains(";")) {
+				comments.append("#");
 				comments.append(in.readLine());
-				comments.append("\n#");
+				comments.append("\n");
 			}
 			
 //			Get new test directory
-			String testDir 	= getNewTest();
+			testDir 	= getNewTest();
 			File testFile	= new File(testDir + "/test");
 
 //			Store the properties for this test
@@ -131,11 +223,30 @@ public class QueryTest {
 			
 			out.close();
 			
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(testDir + "/result.obj")));
+			oos.writeObject(result);
+			oos.close();
 			System.out.println("Test " + testFile.getParentFile().getName() + " created!");
 		} catch(IOException e) {
+			File file = new File(testDir);
+			if(file.exists()) {
+				for(File f : file.listFiles())
+					f.delete();
+				file.delete();
+			}
+			
 			System.err.println(e.getMessage());
+			e.printStackTrace();
 		} catch (ParseException e) {
+			File file = new File(testDir);
+			if(file.exists()) {
+				for(File f : file.listFiles())
+					f.delete();
+				file.delete();
+			}
+			
 			System.err.println(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 
@@ -143,7 +254,7 @@ public class QueryTest {
 	private String getNewTest() {
 		File returnTestDir = null;
 		
-		File dir = new File(queryDirPath);
+		File dir = new File(queryTestsDir);
 		
 		String testPrefix 	= "test";
 		int validTestID 		= 0;
@@ -156,7 +267,7 @@ public class QueryTest {
 			validTestID++;
 		}
 		
-		returnTestDir = new File(queryDirPath + testPrefix + String.format("%03d", validTestID));
+		returnTestDir = new File(queryTestsDir + testPrefix + String.format("%03d", validTestID));
 		if(returnTestDir.exists())
 			throw new IllegalStateException("Error in getNewFile code! Should have picked a directory that does not exist!");
 		returnTestDir.mkdir();
