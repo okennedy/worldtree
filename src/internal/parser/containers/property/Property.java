0925286b90006864d0530ae292acd0b30301bbcb
Guru Prasad <gurupras@buffalo.edu>
2013-09-13 17:31:43 -0400

    Working on in-built properties

--- src/internal/parser/containers/property/Property.java
+++ src/internal/parser/containers/property/Property.java
@@ -1,6 +1,5 @@
 package internal.parser.containers.property;
 
-import internal.parser.TokenCmpOp;
 import internal.parser.containers.IContainer;
 import internal.parser.containers.Reference;
 
@@ -44,5 +43,38 @@ public class Property implements IContainer {
 	 */
 	public String name() {
 		return name;
+	}
+	
+	
+	/**
+	 * Enum containing all the built-in properties<br>
+	 * @author guru
+	 *
+	 */
+	public enum InbuiltPropertyEnum {
+		PASSABLE_EAST("passableeast"),
+		PASSABLE_WEST("passablewest"),
+		;
+		
+		private String property;
+		
+		private InbuiltPropertyEnum(String property) {
+			this.property	= property;
+		}
+		
+		/**
+		 * Check whether a property is built-in
+		 * @param method {@code String} containing the name of the property to check
+		 * @return {@code InbuiltPropertyEnum} corresponding to the specified property <br>
+		 * @throws IllegalStateException if there is no such property
+		 */
+		public static InbuiltPropertyEnum check(String property) {
+			for(InbuiltPropertyEnum rel : values()) {
+				if(rel.property.equalsIgnoreCase(property))
+					return rel;
+			}
+			return null;
+		}
+		
 	}
 }
