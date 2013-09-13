a9248582d41df618885368dc56badc6bd5b7046e
Guru Prasad <gurupras@buffalo.edu>
2013-09-13 09:20:13 -0400

    parser.resolve.Column class now extends Vector instead of ArrayList

--- src/internal/parser/resolve/Column.java
+++ src/internal/parser/resolve/Column.java
@@ -4,13 +4,14 @@ import internal.tree.IWorldTree;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Vector;
 
 /**
  * The Column class is used to store each column of the output produced by the ResolutionEngine.<br>
  * @author guru
  *
  */
-public class Column extends ArrayList<IWorldTree> {
+public class Column extends Vector<IWorldTree> {
 	protected String name;
 	
 	public Column(String name, Collection<IWorldTree> collection) {
