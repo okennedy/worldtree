730af62b383227bebd9b6511e9a50ed49f35209e
Guru Prasad <gurupras@buffalo.edu>
2013-11-12 14:38:36 -0500

    Added methods to RangeSet to provide easy use

--- src/development/com/collection/range/RangeSet.java
+++ src/development/com/collection/range/RangeSet.java
@@ -1,8 +1,11 @@
 package development.com.collection.range;
 
 import internal.parser.TokenCmpOp;
+import internal.parser.containers.Datum;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Random;
 import java.util.TreeSet;
 
 public class RangeSet extends TreeSet<Range> {
@@ -12,7 +15,32 @@ public class RangeSet extends TreeSet<Range> {
 	public RangeSet() {
 		super(new setComparator());
 	}
+
+	public Datum generateRandom() {
+		int size = this.size();
+		int random = (new Random()).nextInt(size);
+		
+		Iterator<Range> iter = this.iterator();
+		for(int i = 0; i < random; i++)
+			iter.next();
+		Range range = iter.next();
+		return range.generateRandom();
+	}
 	
+	public boolean contains(Datum value) {
+		for(Range r : this) {
+			if(r.contains(value))
+				return true;
+		}
+		return false;
+	}
+	
+	public Range get(int index) {
+		Iterator<Range> iter = this.iterator();
+		for(int i = 0; i < index; i++)
+			iter.next();
+		return iter.next();
+	}
 	
 	private static class setComparator implements Comparator<Range> {
 
