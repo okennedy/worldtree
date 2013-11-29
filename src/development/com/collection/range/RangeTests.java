84752e036cc95377caf3966ce2bc38c15b32014a
Guru Prasad <gurupras@buffalo.edu>
2013-11-29 11:53:54 -0500

    Modified RangeTests to work with merged ranges.
    RangeSet class now ensures that there are no overlaps between
    the various ranges that it stores.
    RangeTests now reflects this change

--- src/development/com/collection/range/RangeTests.java
+++ src/development/com/collection/range/RangeTests.java
@@ -33,11 +33,12 @@ public class RangeTests {
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < 5; j++) {
 				Range range = IntegerRange.closed(i, j);
-				set.add(range);
+				if(i == j)
+					set.add(range);
 			}
 		}
-		if(set.size() != 25) 
-			result.append("Failed!\n\t\tRangeSetInsertion failed! Expected 25 elements..found only " + set.size() + "\n");
+		if(set.size() != 5) 
+			result.append("Failed!\n\t\tRangeSetInsertion failed! Expected 5 elements..found only " + set.size() + "\n\n");
 		else
 			result.append("Succeeded!\n");
 	}
@@ -54,10 +55,11 @@ public class RangeTests {
 		}
 		
 //		Basic duplicate test
+		int size = set.size();
 		Range range = set.get(0).clone();
 		set.add(range);
-		if(set.size() != 25) {
-			result.append("Failed!\n\t\tRangeSet size changed when inserting duplicate range!\n");
+		if(set.size() != size) {
+			result.append("Failed!\n\t\tRangeSet size changed when inserting duplicate range!" + "\n\n");
 			failed = true;
 		}
 		
@@ -65,8 +67,8 @@ public class RangeTests {
 		range = set.get(0).clone();
 		range.setLowerBoundType(BoundType.OPEN);
 		set.add(range);
-		if(set.size() != 26) {
-			result.append("Failed!\n\t\tRangeSet size did not change when inserting duplicate range with different lower bound type!\n");
+		if(set.size() != size) {
+			result.append("Failed!\n\t\tRangeSet size changed when inserting duplicate range with different lower bound type!" + "\n\n");
 			failed = true;
 		}
 		set.remove(range);
@@ -75,19 +77,19 @@ public class RangeTests {
 		range = set.get(0).clone();
 		range.setUpperBoundType(BoundType.OPEN);
 		set.add(range);
-		if(set.size() != 26) {
-			result.append("Failed!\n\t\tRangeSet size did not change when inserting duplicate range with different upper bound type!\n");
+		if(set.size() != size) {
+			result.append("Failed!\n\t\tRangeSet size changed when inserting duplicate range with different upper bound type!" + "\n\n");
 			failed = true;
 		}
 		set.remove(range);
 		
-//		Test with different upper bound type
+//		Test with different bound types
 		range = set.get(0).clone();
 		range.setUpperBoundType(BoundType.OPEN);
 		range.setLowerBoundType(BoundType.OPEN);
 		set.add(range);
-		if(set.size() != 26) {
-			result.append("Failed!\n\t\tRangeSet size did not change when inserting duplicate range with different bound types!\n");
+		if(set.size() != size) {
+			result.append("Failed!\n\t\tRangeSet size changed when inserting duplicate range with different bound types!" + "\n\n");
 			failed = true;
 		}
 		set.remove(range);
@@ -98,6 +100,9 @@ public class RangeTests {
 	
 	@Test
 	public void RangeSetMerge() {
+		result.append("\nRangeSetMerge: ");
+		boolean testResult = false;
+		
 		RangeSet set = new RangeSet();
 		set.add(IntegerRange.closed(0, 2));
 		set.add(IntegerRange.closed(4, 12));
@@ -107,15 +112,65 @@ public class RangeTests {
 		
 //		Case 1: R2 lies inside R1 - return R1
 		Range includedRange 	= IntegerRange.open(4, 12);
-		set.add(includedRange);
+		testResult = set.add(includedRange);
+		if(testResult) {
+			result.append("Failed!\n" +
+					"\t\tRangeSet faied on case 1: R2 lies inside R1 - return R1\n" +
+					"\t\tSet is :" + set + "\n" +
+					"\t\trange  :" + includedRange + "\n\n");
+			
+			failed = true;
+		}
+		testResult = false;
 		
 //		Case 2: R1 lies inside R2 - return R2
-		Range includedRange1	= IntegerRange.openClosed(3, 12);
-		set.add(includedRange1);
+		includedRange	= IntegerRange.openClosed(3, 12);
+		testResult = set.add(includedRange);
+		if(testResult) {
+			result.append("Failed!\n" +
+					"\t\tRangeSet faied on case 2: R1 lies inside R2 - return R2\n" +
+					"\t\tSet is :" + set + "\n" +
+					"\t\trange  :" + includedRange + "\n\n");
+			failed = true;
+		}
+		else {
+			testResult = set.contains(includedRange);
+			if(!testResult) {
+				result.append("Failed!\n" +
+						"\t\tRangeSet faied on case 2: R1 lies inside R2 - return R2\n" +
+						"\t\tDid not contain the range that was inserted!\n" +
+						"\t\tSet is :" + set + "\n" +
+						"\t\trange  :" + includedRange + "\n\n");
+				failed = true;
+			}
+		}
+		testResult = false;
+		
 		
 //		Case 3: Partial overlap - return span
 		Range overlapRange		= IntegerRange.open(5, 44);
 		set.add(overlapRange);
-		System.out.println();
+		for(Range range1 : set) {
+			for(Range range2 : set) {
+				if(range1 == range2)
+					continue;
+				else {
+					testResult = range1.intersection(range2) == null ? true : false;
+					if(!testResult) {
+						result.append("Failed!\n" +
+								"\t\tRangeSet faied on case 3: Partial overlap - return span\n" +
+								"\t\tAfter insertion, the set did not eliminate all overlaps\n" +
+								"\t\tOverlap between " + range1 + " and " + range2 + "\n" + 
+								"\t\tSet is :" + set + "\n" +
+								"\t\trange  :" + overlapRange + "\n\n");
+						failed = true;
+					}
+				}
+			}
+		}
+		
+		
+		if(!failed)
+			result.append("Success!\n");
 	}
 }
