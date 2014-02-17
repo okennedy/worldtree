2f55f54b24855ff2b4d465efa654716459d3de90
Guru Prasad <gurupras@buffalo.edu>
2014-02-17 00:05:19 -0500

    Working on resolving dependencies among property definitions

--- src/internal/parser/resolve/constraint/BasicSolver.java
+++ src/internal/parser/resolve/constraint/BasicSolver.java
@@ -2,7 +2,10 @@ package internal.parser.resolve.constraint;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import internal.Helper.Hierarchy;
 import internal.parser.TokenCmpOp;
@@ -153,6 +156,7 @@ public class BasicSolver implements IConstraintSolver {
 	
 	@Override
 	public void pushDownConstraints(IWorldTree node) {
+		sortDefinitions(node);
 		List<IWorldTree> nodes = new ArrayList<IWorldTree>();
 		IMap map = ((IMap) node.root());	//FIXME: Hack
 		nodes.add(map);
@@ -268,7 +272,7 @@ public class BasicSolver implements IConstraintSolver {
 //			Parent has no definition of this property..ignore updating parent
 		}
 	}
-
+	
 	@Override
 	public RangeSet getBounds(IWorldTree node, PropertyDef definition) {
 		return null;
@@ -282,4 +286,122 @@ public class BasicSolver implements IConstraintSolver {
 	public RangeSet processBounds(IWorldTree node, Constraint constraint) {
 		return null;
 	}
+	
+	
+	private void sortDefinitions(IWorldTree root) {
+		Collection<PropertyDef> definitions = root.definitions();
+		
+//		We now find dependencies among property definitions
+		HashMap<Hierarchy, HashMap<Property, PropertyDef>> propertyDefMap = 
+				new HashMap<Hierarchy, HashMap<Property, PropertyDef>>();
+		HashMap<Hierarchy, HashMap<Property, Collection<Property>>> propertyDependencyMap = 
+				new HashMap<Hierarchy, HashMap<Property, Collection<Property>>>();
+		
+		for(Hierarchy level : Hierarchy.values()) {
+			HashMap<Property, Collection<Property>> levelDependencyMap = new HashMap<Property, Collection<Property>>();
+			HashMap<Property, PropertyDef> levelPropertyDefMap = new HashMap<Property, PropertyDef>();
+			for(PropertyDef definition : definitions) {
+				if(!definition.level().equals(level))
+					continue;
+				Property baseProperty = definition.property();
+				levelPropertyDefMap.put(baseProperty, definition);
+				Set<Property> dependencies = new HashSet<Property>();	//TODO: Figure out whether we care about the order here
+				switch(definition.type()) {
+				case AGGREGATE:
+//					TODO: Figure out whether we're only looking at lower levels when we're aggregating..if not, we need some code here
+					break;
+				case BASIC:
+					if(definition.expression() != null) {
+						IExpr expr = definition.expression();
+						while(expr != null) {
+							if(expr.property() != null)
+								dependencies.add(expr.property());
+							expr = expr.subExpr();
+						}
+					}
+					else if(definition.condition() != null) {
+						ICondition condition = definition.condition();
+						while(condition != null) {
+							if(condition.property() != null)
+								dependencies.add(condition.property());
+							condition = condition.subCondition();
+						}
+					}
+					break;
+				case INHERIT:
+//					TODO: Need to implement this
+					break;
+				case RANDOM:
+					break;
+				default:
+					throw new IllegalStateException("Unimplemented PropertyDef type " + definition.type());
+				}
+//				Handle the query
+				IQuery query = definition.query();
+				if(query != null) {
+					while(query != null) {
+						if(query.level().equals(definition.level())) {
+							ICondition queryCondition = query.condition();
+							while(queryCondition != null) {
+								if(queryCondition.property() != null)
+									dependencies.add(queryCondition.property());
+								queryCondition = queryCondition.subCondition();
+							}
+						}
+						query = query.subQuery();
+					}
+				}
+				
+//				We now have all the dependencies
+				levelDependencyMap.put(baseProperty, dependencies);
+			}
+			propertyDefMap.put(level, levelPropertyDefMap);
+			propertyDependencyMap.put(level, levelDependencyMap);
+		}
+		
+		List<PropertyDef> orderedDefinitions = new ArrayList<PropertyDef>();
+		for(Hierarchy level : Hierarchy.values()) {
+			List<Property> levelOrderedProperties = new ArrayList<Property>();
+			resolvePropertyDependencies(propertyDependencyMap.get(level), null, null, levelOrderedProperties);
+//			We now have the order in which definitions need to be instantiated
+			for(Property property: levelOrderedProperties) {
+				HashMap<Property, PropertyDef> localDefMap = propertyDefMap.get(level);
+				orderedDefinitions.add(localDefMap.get(property));
+			}
+		}
+//		At this point, we have an ordered list of definitions
+		assert(definitions.size() == orderedDefinitions.size() && definitions.containsAll(orderedDefinitions)) : 
+			"definitions and orderedDefinitions are not equivalent!\n";
+		root.setDefinitions(orderedDefinitions);
+	}
+	
+	private void resolvePropertyDependencies(HashMap<Property, Collection<Property>> dependencyMap, Property property, 
+			Collection<Property> visited, List<Property> orderedProperties) {
+		if(property == null) {
+//			First call..iterate over map and start solving
+			for(java.util.Map.Entry<Property, Collection<Property>> entry : dependencyMap.entrySet()) {
+				List<Property> newVisited = new ArrayList<Property>();
+				Property baseProperty = entry.getKey();
+				newVisited.add(baseProperty);
+				resolvePropertyDependencies(dependencyMap, baseProperty, newVisited, orderedProperties);
+			}
+		}
+		else {
+			if(dependencyMap.containsKey(property)) {
+				for(Property dependency : dependencyMap.get(property)) {
+					if(visited.contains(dependency)) {
+//						Fatal! We have a circular dependency...
+//						TODO: Add some nice visual showing the circular dependency
+						throw new IllegalStateException("Circular dependency detected!\n");
+					}
+					visited.add(dependency);
+					resolvePropertyDependencies(dependencyMap, dependency, visited, orderedProperties);
+				}
+			}
+			if(!orderedProperties.contains(property))
+				orderedProperties.add(property);
+		}
+	}
+	
+	
 }
