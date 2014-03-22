b94e1fdd67b2cde04949e4c6bf129aebf1ca04cd
Guru Prasad <gurupras@buffalo.edu>
2014-03-22 11:04:59 -0400

    Added validateDefinitions method to ConstraintSolver
    This method is to be used before pushing down constraints
    It ensures that all the constraints that have been defined, have
    definitions at every lower level

--- src/internal/parser/resolve/constraint/ConstraintSolver.java
+++ src/internal/parser/resolve/constraint/ConstraintSolver.java
@@ -1,7 +1,9 @@
 package internal.parser.resolve.constraint;
 
 import development.com.collection.range.RangeSet;
+import internal.Helper.Hierarchy;
 import internal.parser.containers.Constraint;
+import internal.parser.containers.property.Property;
 import internal.parser.containers.property.PropertyDef;
 import internal.tree.IWorldTree;
 
@@ -9,6 +11,7 @@ public class ConstraintSolver {
 	private static IConstraintSolver solver = new BasicSolver();
 	
 	public static void pushDownConstraints(IWorldTree node) {
+		validateDefinitions(node);
 		solver.pushDownConstraints(node);
 	}
 	
@@ -22,5 +25,27 @@ public class ConstraintSolver {
 	
 	public static RangeSet processBounds(IWorldTree node, Constraint constraint) {
 		return solver.processBounds(node, constraint);
+	}
+	
+	private static void validateDefinitions(IWorldTree node) {
+		IWorldTree root = node.root();
+		for(Constraint constraint : root.constraints()) {
+			Property constraintProperty	= constraint.condition().property();
+			Hierarchy constraintLevel	= constraint.level();
+			Hierarchy currentLevel		= constraintLevel;
+			while(currentLevel != null) {
+				boolean validateDefinitions	= false;
+				for(PropertyDef def : root.definitions()) {
+					if(constraintProperty.equals(def.property()) && currentLevel.equals(def.level())) {
+						validateDefinitions	= true;
+						break;
+					}
+				}
+				if(!validateDefinitions) {
+					throw new IllegalStateException("Constraint '" + constraint + "' has no definition at " + currentLevel + " level");
+				}
+				currentLevel = currentLevel.childLevel();
+			}
+		}
 	}
 }
